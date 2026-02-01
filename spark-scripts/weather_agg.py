244b6f5e55abc38a5f74004c0d4ad03393ab515f
Navid SABETE <nevid.dev@gmail.com>
2026-02-01 15:50:42 +0100

    data save in CSV file on Hadoop HDFS

--- spark-scripts/weather_agg.py
+++ spark-scripts/weather_agg.py
@@ -3,10 +3,28 @@ import time
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col, window, avg, count
 from pyspark.sql.types import StructType, StructField, DoubleType, BooleanType, StringType, TimestampType
-
+from hdfs import InsecureClient
+from hdfs.util import HdfsError
+from datetime import datetime
+import json
+import csv
+import io
+import traceback
 
 SPARK_MASTER_URL = os.getenv("SPARK_MASTER_URL")
 KAFKA_BROKER = os.getenv("KAFKA_BROKER")
+
+# Connexion HDFS
+HDFS_URL = "http://namenode:9870"  # URL de ton NameNode
+
+#⚠️ namenode:50070 doit être remplacé par le hostname et port de ton NameNode Docker.
+#Si tu utilises une image Hadoop (pseudo-distribué), regarde le port exposé du NameNode.
+
+HDFS_USER = "root"              # utilisateur HDFS
+#HDFS_USER = "jovyan"
+HDFS_DIR = f"/user/jovyan/weather_agg"
+
+hdfs_client = InsecureClient(HDFS_URL, user=HDFS_USER)
 
 
 def init_spark():
@@ -45,6 +63,7 @@ def main():
     sc = spark.sparkContext
     sc.setLogLevel("WARN")
     print("✅ Spark session started LOG WARN!")
+
     parsed_data = kafka_read(spark)
     agg = parsed_data.groupBy(
         window(col("event_time"), "1 minute")
@@ -52,7 +71,49 @@ def main():
         avg("temperature").alias("avg_temp_c"),
         count(col("high_wind_alert")).alias("alert_count")
         )
-    agg.show(truncate=False)
+    results = agg.collect()
+    buffer = io.StringIO()
+    writer = csv.writer(buffer)
+
+    # En-têtes CSV
+    writer.writerow([
+        "window_start",
+        "window_end",
+        "avg_temp_c",
+        "alert_count"
+    ])
+
+    # Lignes
+    for row in results:
+        writer.writerow([
+            row["window"].start,
+            row["window"].end,
+            row["avg_temp_c"],
+            row["alert_count"]
+        ])
+
+    print("📦 VERIFICATION DOSSIER")
+    try:
+        hdfs_client.status(HDFS_DIR)
+        print(f"✅ HDFS directory exists: {HDFS_DIR}")
+    except HdfsError:
+        print(f"📁 Creating HDFS directory: {HDFS_DIR}")
+    hdfs_client.makedirs(HDFS_DIR)
+
+    print("📂 HDFS root content:", hdfs_client.list("/user/jovyan"))
+
+    # Nom de fichier horodaté
+    ts = datetime.utcnow().strftime("%Y%m%dT%H%M%SZ")
+    filename = f"weather_agg_{ts}.csv"
+    hdfs_path = f"{HDFS_DIR}/{filename}"
+
+    # Écriture HDFS
+    with hdfs_client.write(hdfs_path, overwrite=False, encoding="utf-8") as writer:
+        writer.write(buffer.getvalue())
+
+    print("✅ CSV aggregation saved to HDFS:", hdfs_path)
+    
+    #agg.show(truncate=False)
 
     
 if __name__ == "__main__":
